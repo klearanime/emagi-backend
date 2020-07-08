9df8cd2ebede91e2c6706b5e832805307da4fd12
Crystal Young <crystal.young@codeimmersives.com>
2020-07-08 15:15:33 -0400

    done

---
+++ emagify.js
@@ -0,0 +1,17 @@
+const emagify = function (emoji) {
+    const randomElement = function(arr) {
+        let randomNumber = Math.floor((Math.random() * arr.length));
+        return arr[randomNumber]
+    }
+    console.log(randomNumber)
+}  
+
+
+
+module.exports = emagify;
+
+// Methods Worth Looking Into To Help With These Tasks
+// Array's .includes
+// String's .includes
+// Array's .indexOf
+// Math's .floor and .random
